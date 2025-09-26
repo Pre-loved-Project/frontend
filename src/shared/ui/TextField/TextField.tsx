e3f08d7f94f585870ba75098ffb11cd8d4cdcd09
Taewoo Park <taewoo9240@gmail.com>
2025-09-26 10:31:04 +0900

    feat: TextField에 숨김 옵션 및 숨김 on/off 버튼 추가

--- src/shared/ui/TextField/TextField.tsx
+++ src/shared/ui/TextField/TextField.tsx
@@ -1,25 +1,70 @@
 "use client";
 
 import React, { InputHTMLAttributes, useState } from "react";
-import cn from "@/shared/lib/cn"; // ✅ cn 유틸 import
+import cn from "@/shared/lib/cn";
+import Image from "next/image";
+import eyeIcon from "@/shared/images/eye.svg"
+import eyeOffIcon from "@/shared/images/eye-off.svg"
 
-interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
-  width?: string | number;
-  height?: string | number;
-  error?: boolean;
+type TextFieldSize =  "sm" | "md" | "lg";
+type TextFieldWidthSize = "long" | "short";
+
+interface TextFieldProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "size"> {
+  fieldSize?: TextFieldSize;
+  widthSize?: TextFieldWidthSize;
+  isError?: boolean;
+  isHiddenable?: boolean;
 }
 
-export const TextField = (props: TextFieldProps) => {
-  const { width, height, error, className, ...rest } = props;
+const styleMap: Record<
+  TextFieldSize,
+  {
+    widthMap: Record<TextFieldWidthSize, number>;
+    height: number;
+    textSize: string;
+    iconSize: { w: number; h: number };
+  }
+> = {
+  lg: {
+    widthMap: { long: 640, short: 400 },
+    height: 70,
+    textSize: "text-[16px] placeholder:text-[16px]",
+    iconSize: { w: 24, h: 24 },
+  },
+  md: {
+    widthMap: { long: 440, short: 360 },
+    height: 55,
+    textSize: "text-[14px] placeholder:text-[14px]",
+    iconSize: { w: 22, h: 22 },
+  },
+  sm: {
+    widthMap: { long: 335, short: 335 },
+    height: 55,
+    textSize: "text-[14px] placeholder:text-[14px]",
+    iconSize: { w: 22, h: 22 },
+  },
+};
 
+export const TextField = ({
+  fieldSize = "lg",
+  widthSize = "long",
+  isError = false,
+  isHiddenable = false,
+  className,
+  ...rest
+} : TextFieldProps) => {
   const [text, setText] = useState("");
   const [focused, setFocused] = useState(false);
+  const [hidden, setHidden] = useState(isHiddenable); //비밀번호 시 숨겨진 상태로 시작
+
+  const { widthMap, height, textSize, iconSize} = styleMap[fieldSize];
+  const width = widthMap[widthSize];
 
   return (
     <div
       className={cn(
         "flex items-center rounded-lg transition-colors bg-black-800 px-[20px] py-[23px]",
-        error
+        isError
           ? "border border-red"
           : focused
           ? "border border-blue"
@@ -29,24 +74,39 @@ export const TextField = (props: TextFieldProps) => {
     >
       <input
         {...rest}
-        value={text}
+        type={hidden ? "password" : "text"}
+        value={rest.value ?? text} //부모에서 내용 control 가능!
         onChange={(e) => {
           setText(e.target.value);
-          props.onChange?.(e);
+          rest.onChange?.(e); //text 수정 시 handling
         }}
         className={cn(
+          textSize,
           "flex-1 bg-transparent outline-none text-white",
           className
         )}
         onFocus={(e) => {
           setFocused(true);
-          props.onFocus?.(e);
         }}
         onBlur={(e) => {
           setFocused(false);
-          props.onBlur?.(e);
         }}
       />
+
+      {isHiddenable && (
+        <button
+          type="button"
+          onClick={() => setHidden((prev) => !prev)}
+          className="ml-2"
+        >
+          <Image
+            src={hidden ? eyeIcon : eyeOffIcon}
+            alt={hidden ? "hidden" : "visible"}
+            width={iconSize.w}
+            height={iconSize.h}
+          />
+        </button>
+      )}
     </div>
   );
 };
