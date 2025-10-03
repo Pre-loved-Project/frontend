df3d018aa35e93c0869f46d579739c3e9bdc1cae
ghdtnals <hongsumin58@gmail.com>
2025-10-03 17:38:39 +0900

    feat: 헤더 이동용 임시 페이지 생성

---
+++ src/app/my/page.tsx
@@ -0,0 +1,17 @@
+const Mypage = () => {
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <section></section>
+      <section>
+        <h2>내가 올린 상품 목록</h2>
+        <div></div>
+      </section>
+      <section>
+        <h2>관심 상품</h2>
+        <div></div>
+      </section>
+    </main>
+  );
+};
+
+export default Mypage;
