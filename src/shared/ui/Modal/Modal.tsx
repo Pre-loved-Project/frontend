1dbe651000dd89e798359593bf7868b5a9a8cbf0
Taewoo Park <taewoo9240@gmail.com>
2025-10-07 13:58:04 +0900

    style: modal 컴포넌트 tailwind 반응형 적용

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -4,49 +4,43 @@ import React from "react";
 import cn from "@/shared/lib/cn";
 import Button from "../Button/Button";
 
-type ModalSize = "sm" | "md" | "lg";
-
 interface ModalProps {
-  size?: ModalSize;
   message: string;
   buttonText?: string;
   onClick: () => void;
+  className: string;
 }
 
-const sizeMap: Record<
-  ModalSize,
-  {
-    width: number;
-    height: number;
-    buttonSize: "sm" | "md" | "lg";
-    textSize: string;
-  }
-> = {
-  sm: { width: 400, height: 200, buttonSize: "sm", textSize: "text-base" },
-  md: { width: 500, height: 200, buttonSize: "md", textSize: "text-lg" },
-  lg: { width: 500, height: 200, buttonSize: "md", textSize: "text-lg" },
-};
-
 export const Modal = ({
-  size = "md",
   message,
   buttonText = "확인",
   onClick,
+  className,
 }: ModalProps) => {
-  const { width, height, buttonSize, textSize } = sizeMap[size];
-
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
       <div
         className={cn(
           "bg-black-900 rounded-lg shadow-lg flex flex-col items-center justify-center gap-10 p-6 text-center",
+          "w-[320px] h-[180px]",
+          "md:w-[420px] md:h-[190px]",
+          "xl:w-[500px] xl:h-[200px]",
+          className,
         )}
-        style={{ width, height }}
       >
-        <p className={cn("text-white whitespace-pre-line", textSize)}>
+        <p
+          className={cn(
+            "text-white whitespace-pre-line",
+            "text-[14px] md:text-[16px] xl:text-[18px]",
+          )}
+        >
           {message}
         </p>
-        <Button variant="primary" size={buttonSize} onClick={onClick}>
+        <Button
+          variant="primary"
+          onClick={onClick}
+          className={cn("w-[200px]", "md:w-[300px]", "xl:w-[400px]")}
+        >
           {buttonText}
         </Button>
       </div>
