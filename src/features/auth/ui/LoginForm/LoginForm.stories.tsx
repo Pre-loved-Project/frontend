22d547d26e036653869c49dd52f8dcde870fdf8d
Taewoo Park <taewoo9240@gmail.com>
2025-10-07 18:51:44 +0900

    style: loginForm tailwind 반응형 적용

--- src/features/auth/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/auth/ui/LoginForm/LoginForm.stories.tsx
@@ -5,35 +5,10 @@ const meta: Meta<typeof LoginForm> = {
   title: "Auth/LoginForm",
   component: LoginForm,
   tags: ["autodocs"],
-  argTypes: {
-    size: {
-      control: { type: "radio" },
-      options: ["sm", "md", "lg"],
-      description: "폼 전체 Input과 Button 크기를 조절합니다",
-    },
-  },
 };
 export default meta;
 
 type Story = StoryObj<typeof LoginForm>;
 
 // 기본 (Large size)
-export const Default: Story = {
-  args: {
-    size: "lg",
-  },
-};
-
-// Small size
-export const Small: Story = {
-  args: {
-    size: "sm",
-  },
-};
-
-// Medium size
-export const Medium: Story = {
-  args: {
-    size: "md",
-  },
-};
+export const Default: Story = {};
