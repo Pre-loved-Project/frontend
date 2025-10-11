2c76d0a1e32653ecc41b25b03efdac0a437ad642
taew0o <taew0o@users.noreply.github.com>
2025-10-11 06:07:20 +0000

    deploy: 42c20f83b86558038e9994c433371a6ed1f626ea

---
+++ assets/Button.stories-B3h6-7tT.js
@@ -0,0 +1,16 @@
+import{B as o}from"./Button-BJJsHKll.js";import"./jsx-runtime-bukIPEy0.js";import"./iframe-9S3Ogy7W.js";import"./preload-helper-B4AOKawx.js";import"./cn-DWDuF9m2.js";const{fn:t}=__STORYBOOK_MODULE_TEST__,m={title:"Components/Button",component:o,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{variant:{control:{type:"radio"},options:["primary","secondary","tertiary"]},disabled:{control:"boolean"}},args:{onClick:t()}},r={args:{variant:"primary",children:"Primary"}},a={args:{variant:"secondary",children:"Secondary"}},e={args:{variant:"tertiary",children:"Tertiary"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "primary",
+    children: "Primary"
+  }
+}`,...r.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary",
+    children: "Secondary"
+  }
+}`,...a.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "tertiary",
+    children: "Tertiary"
+  }
+}`,...e.parameters?.docs?.source}}};const p=["Primary","Secondary","Tertiary"];export{r as Primary,a as Secondary,e as Tertiary,p as __namedExportsOrder,m as default};
---
+++ assets/jsx-runtime-bukIPEy0.js
@@ -0,0 +1,1 @@
+import{d as r}from"./iframe-9S3Ogy7W.js";var s=r();export{s as j};
