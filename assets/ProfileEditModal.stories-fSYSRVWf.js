f91c699ad6e21934e95c3c8c048ba5aceebc4cdb
ghdtnals <ghdtnals@users.noreply.github.com>
2025-10-27 13:57:57 +0000

    deploy: 60260e9110f8734ce6a599c75e868ac56c6b4edc

---
+++ assets/ProfileEditModal.stories-fSYSRVWf.js
@@ -0,0 +1,16 @@
+import{j as e}from"./jsx-runtime-DuQ7uIwd.js";import{r as a}from"./iframe-DaT0DpmT.js";import{b as r}from"./fetcher-CU9G3k9g.js";import"./preload-helper-B4AOKawx.js";import"./auth.store-CHGqCpFc.js";import"./Modal-C-ekly4K.js";import"./cn-DWDuF9m2.js";import"./Button-KCwFyi-b.js";import"./ProfileImageChangeInput-BbYgNwMT.js";import"./image-DhPH5JbM.js";import"./use-merged-ref-DHGlSaDd.js";import"./Input-Do9DU_A3.js";import"./TextField-Ckj_KvR9.js";import"./TextBox-CjpNy8oj.js";import"./DropDown-QU-MimCI.js";import"./delete-DpU2ogCQ.js";const Q={title:"User/ProfileEditModal",component:r,tags:["autodocs"],parameters:{docs:{story:{inline:!1,iframeHeight:900}}},argTypes:{onClose:{action:"closed"},onSave:{action:"saved"}}},t={render:s=>{const[o,n]=a.useState(!0);return e.jsxs(e.Fragment,{children:[o&&e.jsx(r,{...s,imageUrl:"https://i.namu.wiki/i/bWtUr_r8eY7ikAhSgpZY0kga1QCDgZIsQI9P3UDmIDKqBJ67zQGZg95H2Svkph_k6PkQ2w_HRXj55lF9BSZD07VqH2PUdFLKWDm880fd0dIgk9nsC3vamlu0iPsjnPe4EOt6cGF7u0wWKfyYWjsaRHQRjBwFu5wmUD1RZtcxQyE.webp",nickname:"홍길동",introduction:"안녕하세요. 저는 테스트 사용자입니다.",category:"전자제품/가전제품",onClose:()=>{console.log("모달 닫기"),n(!1)},onSave:()=>{console.log("저장 클릭")}}),!o&&e.jsx("button",{onClick:()=>n(!0),className:"mt-5 rounded bg-blue-500 px-4 py-2 text-white",children:"모달 열기"})]})}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  render: args => {
+    const [open, setOpen] = useState(true);
+    return <>
+        {open && <ProfileEditModal {...args} imageUrl="https://i.namu.wiki/i/bWtUr_r8eY7ikAhSgpZY0kga1QCDgZIsQI9P3UDmIDKqBJ67zQGZg95H2Svkph_k6PkQ2w_HRXj55lF9BSZD07VqH2PUdFLKWDm880fd0dIgk9nsC3vamlu0iPsjnPe4EOt6cGF7u0wWKfyYWjsaRHQRjBwFu5wmUD1RZtcxQyE.webp" nickname="홍길동" introduction="안녕하세요. 저는 테스트 사용자입니다." category="전자제품/가전제품" onClose={() => {
+        console.log("모달 닫기");
+        setOpen(false);
+      }} onSave={() => {
+        console.log("저장 클릭");
+      }} />}
+        {!open && <button onClick={() => setOpen(true)} className="mt-5 rounded bg-blue-500 px-4 py-2 text-white">
+            모달 열기
+          </button>}
+      </>;
+  }
+}`,...t.parameters?.docs?.source}}};const S=["Main"];export{t as Main,S as __namedExportsOrder,Q as default};
--- assets/jsx-runtime-ibvY5E17.js
+++
@@ -1,1 +0,0 @@
-import{d as r}from"./iframe-CM6bk4Uq.js";var s=r();export{s as j};
