959ea40eb3ca40c34ba5cbb760db6c8c1fe28542
taew0o <taew0o@users.noreply.github.com>
2025-11-05 07:58:32 +0000

    deploy: 11abc2bf392eba2e0b6e3ab356a03d3693996f7a

---
+++ assets/ProfileEditModal.stories-CssWkncM.js
@@ -0,0 +1,16 @@
+import{j as e}from"./jsx-runtime-CeEVroB2.js";import{r as a}from"./iframe-tiG3KD05.js";import{b as r}from"./fetcher-BH0xlI3f.js";import"./preload-helper-B4AOKawx.js";import"./auth.store-CFY-RV1j.js";import"./Modal-CpoGRJz6.js";import"./cn-DWDuF9m2.js";import"./Button-DVl-jrM-.js";import"./ProfileImageChangeInput-5LbaPb8b.js";import"./image-B0sY_i1r.js";import"./Input-Cd3C9Kkf.js";import"./TextField-kRL5-EQC.js";import"./TextBox-GCdvCFOW.js";import"./DropDown-Loj8Afxy.js";const b={title:"User/ProfileEditModal",component:r,tags:["autodocs"],parameters:{docs:{story:{inline:!1,iframeHeight:900}}},argTypes:{onClose:{action:"closed"},onSave:{action:"saved"}}},t={render:s=>{const[o,n]=a.useState(!0);return e.jsxs(e.Fragment,{children:[o&&e.jsx(r,{...s,imageUrl:"https://i.namu.wiki/i/bWtUr_r8eY7ikAhSgpZY0kga1QCDgZIsQI9P3UDmIDKqBJ67zQGZg95H2Svkph_k6PkQ2w_HRXj55lF9BSZD07VqH2PUdFLKWDm880fd0dIgk9nsC3vamlu0iPsjnPe4EOt6cGF7u0wWKfyYWjsaRHQRjBwFu5wmUD1RZtcxQyE.webp",nickname:"홍길동",introduction:"안녕하세요. 저는 테스트 사용자입니다.",category:"전자제품/가전제품",onClose:()=>{console.log("모달 닫기"),n(!1)},onSave:()=>{console.log("저장 클릭")}}),!o&&e.jsx("button",{onClick:()=>n(!0),className:"mt-5 rounded bg-blue-500 px-4 py-2 text-white",children:"모달 열기"})]})}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  render: args => {
+    const [open, setOpen] = useState(true);
+    return <>
+        {open && <ProfileEditModal {...args} imageUrl="https://i.namu.wiki/i/bWtUr_r8eY7ikAhSgpZY0kga1QCDgZIsQI9P3UDmIDKqBJ67zQGZg95H2Svkph_k6PkQ2w_HRXj55lF9BSZD07VqH2PUdFLKWDm880fd0dIgk9nsC3vamlu0iPsjnPe4EOt6cGF7u0wWKfyYWjsaRHQRjBwFu5wmUD1RZtcxQyE.webp" nickname="홍길동" introduction="안녕하세요. 저는 테스트 사용자입니다." category="전자제품/가전제품" onClose={() => {
+        console.log("모달 닫기");
+        setOpen(false);
+      }} onSave={() => {
+        console.log("저장 클릭");
+      }} />}
+        {!open && <button onClick={() => setOpen(true)} className="mt-5 rounded bg-blue-500 px-4 py-2 text-white">
+            모달 열기
+          </button>}
+      </>;
+  }
+}`,...t.parameters?.docs?.source}}};const D=["Main"];export{t as Main,D as __namedExportsOrder,b as default};
---
+++ assets/jsx-runtime-CeEVroB2.js
@@ -0,0 +1,1 @@
+import{e as r}from"./iframe-tiG3KD05.js";var s=r();export{s as j};
