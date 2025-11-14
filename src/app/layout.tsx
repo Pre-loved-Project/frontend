e24008d56c91ace8acaca863a9c8addf78630224
ghdtnals <hongsumin58@gmail.com>
2025-11-14 21:13:18 +0900

    refactor: 클라이언트 레이아웃 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import Header from "@/widgets/header/ui/Header";
 import { ModalContainer } from "@/shared/ui/ModalContainer/ModalContainer";
 import ChatContainer from "@/features/chat/ui/ChatContainer";
 import { ReactQueryProvider } from "@/shared/lib/provider";
-import { ClientLayout } from "@/shared/lib/ClientLayout";
 
 const myFont = localFont({
   src: "../shared/fonts/PretendardVariable.woff2",
@@ -26,27 +25,25 @@ export default function RootLayout({
     <html lang="kr" className={myFont.className}>
       <body className="mx-auto max-w-[1200px] bg-[#1c1c22] pt-[70px] md:pt-[80px] xl:pt-[100px]">
         <ReactQueryProvider>
-          <ClientLayout>
-            <Suspense fallback={null}>
-              <Header />
-            </Suspense>
+          <Suspense fallback={null}>
+            <Header />
+          </Suspense>
 
-            <Suspense
-              fallback={<div className="p-4 text-gray-400">Loading...</div>}
-            >
-              {children}
-            </Suspense>
+          <Suspense
+            fallback={<div className="p-4 text-gray-400">Loading...</div>}
+          >
+            {children}
+          </Suspense>
 
-            <div id="modal-root" />
+          <div id="modal-root" />
 
-            <Suspense fallback={null}>
-              <ChatContainer />
-            </Suspense>
+          <Suspense fallback={null}>
+            <ChatContainer />
+          </Suspense>
 
-            <Suspense fallback={null}>
-              <ModalContainer />
-            </Suspense>
-          </ClientLayout>
+          <Suspense fallback={null}>
+            <ModalContainer />
+          </Suspense>
         </ReactQueryProvider>
       </body>
     </html>
