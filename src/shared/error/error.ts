85b844e26dd6d7dec5456222cadd7c9af3476162
Taewoo Park <taewoo9240@gmail.com>
2025-11-26 20:32:40 +0900

    feat: 에러 타입 정의 및 에러 처리 함수 생성

---
+++ src/shared/error/error.ts
@@ -0,0 +1,27 @@
+export class BaseError extends Error {
+  constructor(
+    message: string,
+    public readonly status?: number,
+  ) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+export class AuthorizationError extends BaseError {
+  constructor(message = "인증이 필요합니다.") {
+    super(message, 401);
+  }
+}
+
+export class NotFoundError extends BaseError {
+  constructor(message = "요청한 리소스를 찾을 수 없습니다.") {
+    super(message, 404);
+  }
+}
+
+export class ServerError extends BaseError {
+  constructor(message = "서버 오류가 발생했습니다.") {
+    super(message, 500);
+  }
+}
