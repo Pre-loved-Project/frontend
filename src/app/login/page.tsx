64a85ada0b2ddc2bfa109d8714271479cef47d85
Taewoo Park <taewoo9240@gmail.com>
2025-11-27 12:05:44 +0900

    feat: 로그인 페이지 세션 만료 확인 처리 로직 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { LoginForm } from "@/features/auth/ui/LoginForm/LoginForm";
 import { Modal } from "@/shared/ui/Modal/Modal";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useAuthStore } from "@/features/auth/model/auth.store";
-
-export default function SignUpPage() {
+import { useChatStore } from "@/features/chat/model/chat.store";
+export default function LoginPage() {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const expired = searchParams.get("expired");
+
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const { setIsLogined } = useAuthStore();
+  const { setIsLogined, logout } = useAuthStore();
+  const { unmount } = useChatStore();
+
+  useEffect(() => {
+    if (expired === "true") {
+      unmount();
+      logout();
+      setErrorMessage("세션이 만료되었습니다.\n다시 로그인해주세요.");
+    }
+  }, [expired]);
+
   return (
     <div className="flex min-h-[calc(100vh-70px)] flex-col items-center justify-center px-4 md:min-h-[calc(100vh-80px)] xl:min-h-[calc(100vh-100px)]">
       <h1 className="mb-8 text-2xl font-bold text-white">로그인</h1>
