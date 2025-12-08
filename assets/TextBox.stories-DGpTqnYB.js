3b22118c68ffdc34caf7da2e061fc5ad06e2df3b
ghdtnals <ghdtnals@users.noreply.github.com>
2025-12-08 06:47:54 +0000

    deploy: 5a589f1cc42d430c2aa21c848e832000611dc25f

---
+++ assets/TextBox.stories-DGpTqnYB.js
@@ -0,0 +1,9 @@
+import{j as a}from"./jsx-runtime-Dr1U99Bd.js";import{r as l}from"./iframe-bDca9Gqz.js";import{T as r}from"./TextBox-CvxTeNX-.js";import"./preload-helper-B4AOKawx.js";import"./cn-DWDuF9m2.js";const d={title:"COMPONENTS/TextBox",component:r,tags:["autodocs"],parameters:{docs:{story:{inline:!1,iframeHeight:300}}},argTypes:{placeholder:{control:"text"},maxLength:{control:"number"}},args:{placeholder:"자기소개를 입력하세요...",maxLength:300}},e={render:s=>{const[t,o]=l.useState("");return a.jsxs("div",{className:"flex flex-col gap-4",children:[a.jsx(r,{...s,value:t,onChange:n=>o(n.target.value)}),a.jsxs("div",{className:"text-black-900",children:["현재 입력 값: ",t||"없음"]})]})}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: args => {
+    const [value, setValue] = useState("");
+    return <div className="flex flex-col gap-4">
+        <TextBox {...args} value={value} onChange={e => setValue(e.target.value)} />
+        <div className="text-black-900">현재 입력 값: {value || "없음"}</div>
+      </div>;
+  }
+}`,...e.parameters?.docs?.source}}};const p=["Main"];export{e as Main,p as __namedExportsOrder,d as default};
---
+++ assets/index-XrlMb0ad.js
@@ -0,0 +1,1 @@
+import{s as r,n as o}from"./iframe-bDca9Gqz.js";var t=r();const s=o(t);export{s as F,t as r};
